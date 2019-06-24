2ad10c9b46b3f56ea87f9e3ec38f9686f739664d
Erik Loaiza <erik.loaiza@uao.edu.co>
2019-06-24 11:56:58 -0500

    Added Https & Pdf Load

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    devServer: {
+      https: true
+    },
+    chainWebpack: config => {
+      config.module
+        .rule('vue')
+        .use('vue-loader')
+          .loader('vue-loader')
+          .tap(options => {
+            // modify the options...
+            return options
+          })
+    
+      config.module.rule('pdf')
+        .test(/\.pdf$/)
+        .use('file-loader').loader('file-loader')
+  
+    },
+    configureWebpack: config => { config.output.globalObject = "this"; }
+  };
+  
